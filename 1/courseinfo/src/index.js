da523cd7f5e05bd5da71ed7d692b0933fa4a5ea2
Dragan Ćulibrk <draganculibrk9@gmail.com>
2020-10-13 21:02:36 +0200

    Complete exercises 1.1-1.2

---
+++ 1/courseinfo/src/index.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = (props) => {
+    return (
+        <h1>{props.name}</h1>
+    )
+}
+
+const Part = (props) => {
+    return (
+        <p>
+            {props.name} {props.exercises}
+        </p>
+    )
+}
+
+const Content = (props) => {
+    const parts = [];
+
+    for (const part of props.parts) {
+        parts.push(
+            <Part name={part.name} exercises={part.exercises}/>
+        )
+    }
+
+    return (
+        <div>
+            {parts}
+        </div>
+    )
+}
+
+const Total = (props) => {
+    let total = 0;
+
+    for (const part of props.parts) {
+        total += part.exercises;
+    }
+
+    return (
+        <p>Number of exercises {total}</p>
+    )
+}
+
+const App = () => {
+    const course = 'Half Stack application development'
+    const parts = [
+        {
+            name: 'Fundamentals of React',
+            exercises: 10
+        },
+        {
+            name: 'Using props to pass data',
+            exercises: 7
+        },
+        {
+            name: 'State of a component',
+            exercises: 14
+        }
+    ]
+
+    return (
+        <div>
+            <Header name={course}/>
+            <Content parts={parts}/>
+            <Total parts={parts}/>
+        </div>
+    )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
