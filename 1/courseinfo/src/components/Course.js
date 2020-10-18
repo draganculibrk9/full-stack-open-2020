a600c3943adda93cf74ed0c82da40f4c4d065674
Dragan Ćulibrk <draganculibrk9@gmail.com>
2020-10-18 14:00:25 +0200

    Complete exercises 2.1-2.5

---
+++ 1/courseinfo/src/components/Course.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const Header = (props) => {
+    return (
+        <h1>{props.name}</h1>
+    )
+}
+
+const Part = (props) => {
+    return (
+        <p>
+            {props.name} {props.exercises}
+        </p>
+    )
+}
+
+const Content = ({parts}) => {
+    return (
+        <div>
+            {parts.map(part => <Part name={part.name} exercises={part.exercises}/>)}
+        </div>
+    )
+}
+
+const Total = ({parts}) => {
+    const total = parts.reduce((accumulator, currentValue) => accumulator + currentValue.exercises, 0)
+    return (
+        <p>
+            <b>
+                total of {total} exercises
+            </b>
+        </p>
+    )
+}
+
+const Course = ({course}) => {
+    return (
+        <div>
+            <Header name={course.name}/>
+            <Content parts={course.parts}/>
+            <Total parts={course.parts}/>
+        </div>
+    )
+}
+
+export default Course
