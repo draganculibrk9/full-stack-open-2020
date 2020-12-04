d877b4a087b99b478c03c2ff1047ce25f56b87f2
Dragan Ćulibrk <draganculibrk9@gmail.com>
2020-12-04 20:24:19 +0100

    Finish exercise 9.7

--- 9/bmi/exerciseCalculator.ts
+++ 9/bmi/exerciseCalculator.ts
@@ -15,24 +15,6 @@ interface ExerciseCalculatorInput {
     dailyHours: number[];
 }
 
-const parseExerciseCalculatorArguments = (args: string[]): ExerciseCalculatorInput => {
-    if (args.length < 4) throw new Error('Not enough arguments');
-
-    const target = Number(args[2]);
-    const dailyHours = args.slice(3).map(h => Number(h));
-
-    if (isNaN(target) || !dailyHours.every(h => !isNaN(h))) {
-        throw new Error('Provided values were not numbers!');
-    } else if (target < 0 || !dailyHours.every(h => h >= 0)) {
-        throw new Error('Provided values must be greater than zero or zero!');
-    }
-
-    return {
-        target,
-        dailyHours
-    };
-};
-
 const calculateRating = (dailyHours: number[], target: number): number => {
     const ratio = dailyHours.filter(h => h >= target).length / dailyHours.length;
 
@@ -56,7 +38,7 @@ const getRatingDescription = (rating: number): RatingDescription => {
     }
 };
 
-const calculateExercises = (exerciseCalculatorInput: ExerciseCalculatorInput): Result => {
+export const calculateExercises = (exerciseCalculatorInput: ExerciseCalculatorInput): Result => {
     const dailyHours = exerciseCalculatorInput.dailyHours;
     const target = exerciseCalculatorInput.target;
 
@@ -71,11 +53,3 @@ const calculateExercises = (exerciseCalculatorInput: ExerciseCalculatorInput): R
         average: dailyHours.reduce((a, b) => a + b, 0) / dailyHours.length
     };
 };
-
-try {
-    console.log(calculateExercises(parseExerciseCalculatorArguments(process.argv)));
-} catch (e) {
-    if (e instanceof Error) {
-        console.log('Error: ', e.message);
-    }
-}
--- 9/bmi/index.ts
+++ 9/bmi/index.ts
@@ -1,18 +1,23 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 import {calculateBMI} from './bmiCalculator';
+import {calculateExercises} from './exerciseCalculator';
+import cors from 'cors';
 
 const app = express();
 
-app.get('/hello', (_req, res) => {
+app.use(cors());
+app.use(express.json());
+
+app.get('/hello', (_req: Request, res: Response) => {
     res.send('Hello Full Stack!');
 });
 
-app.get('/bmi', (req, res) => {
+app.get('/bmi', (req: Request, res: Response) => {
     const height = Number(req.query.height);
     const weight = Number(req.query.weight);
 
     if (isNaN(height) || isNaN(weight) || height <= 0 || weight <= 0) {
-        res.status(400)
+        return res.status(400)
             .json({
                 error: "malformated parameters"
             });
@@ -20,11 +25,38 @@ app.get('/bmi', (req, res) => {
 
     const value = calculateBMI({height, weight});
 
-    res.json({
+    return res.json({
         weight,
         height,
         bmi: value
     });
+});
+
+app.post('/exercises', (req: Request, res: Response) => {
+    const exerciseInput: Record<string, unknown> = req.body as Record<string, unknown>;
+
+    if (exerciseInput.target === undefined || exerciseInput.target === null || !exerciseInput.daily_exercises) {
+        return res.status(400)
+            .json({
+                error: 'Parameters missing'
+            });
+    }
+
+    const target = Number(exerciseInput.target);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const daily_exercises = (exerciseInput.daily_exercises as []).map((d: any) => Number(d));
+
+    if (isNaN(target) || target < 0 || !daily_exercises.every((d: number) => !isNaN(d) && d >= 0)) {
+        return res.status(400)
+            .json({
+                error: 'Malformatted parameters'
+            });
+    }
+
+    return res.json(calculateExercises({
+        dailyHours: daily_exercises,
+        target
+    }));
 });
 
 const PORT = 3003;
